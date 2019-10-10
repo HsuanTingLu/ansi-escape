23928e61c69832ab40305338431475fdf23e72fd
Hsuan Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2019-10-10 16:54:39 +0800

    Separate 8-bit colour codes

--- aesc/color/color256.hpp
+++ aesc/color/color256.hpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2019  Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
  *
- * Wrap some CSI escape codes with string streams
+ * Wrap 8-bit color codes within general output streams
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,21 +18,21 @@
  *
  */
 
-#ifndef AESC_CSI_HPP_
-#define AESC_CSI_HPP_
+#ifndef AESC_COLOR_COLOR256_HPP_
+#define AESC_COLOR_COLOR256_HPP_
 
-#include <iostream>
-#include "cursor.hpp"
-#include "manipulator.hpp"
-#include "SGR.hpp"
-#include "color/color.hpp"
-#include "color/color256.hpp"
-#include "color/truecolor.hpp"
+#include "../aesc.hpp"
+#include "../manipulator.hpp"
 
 namespace aesc {  // Ansi Escape Terminal
 
-namespace truecolor {
-
+namespace color256 {
+// 8-bit, 256 colors
+/* <Predefined 256 colors>
+ * <n>:
+ * 16- 231:  6×6×6 cube (216 colors): 16 + 36×r + 6×g + b (0≤r,g,b≤5)
+ * 232-255:  grayscale from black to white in 24 steps
+ */
 namespace RGB {
 manipulator::smanipiii foreground(const int r = 0, const int g = 0,
                                   const int b = 0);
@@ -40,7 +40,16 @@ manipulator::smanipiii background(const int r = 0, const int g = 0,
                                   const int b = 0);
 }  // namespace RGB
 
-}  // namespace truecolor
+namespace grey {
+/*
+ * 24 step grey:
+ * <n> ranges from 0 to 23, from white to black
+ */
+manipulator::smanip foreground(const int n = 1);
+manipulator::smanip background(const int n = 1);
+}  // namespace grey
+
+}  // namespace color256
 
 }  // namespace aesc
 
