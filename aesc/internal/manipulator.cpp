c20bd7fbbbd088d0e3cbb91daf98150459a4932a
Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2020-01-09 15:06:50 +0800

    Hide some more private functions

--- aesc/internal/manipulator.cpp
+++ aesc/internal/manipulator.cpp
@@ -23,13 +23,24 @@
 namespace aesc {  // Ansi Escape Terminal
 
 namespace manipulator {
+
 smanip::smanip(std::ostream& (*ff)(std::ostream&, const int), const int ii)
     : f{ff}, i{ii} {}
+
+std::ostream& operator<<(std::ostream& os, const smanip& m) {
+    m.f(os, m.i);  // call m’s function with m’s stored value i
+    return os;
+}
 
 smanipiii::smanipiii(std::ostream& (*ff)(std::ostream&, const int, const int,
                                          const int),
                      const int i1, const int i2, const int i3)
     : f{ff}, i1{i1}, i2{i2}, i3{i3} {}
+
+std::ostream& operator<<(std::ostream& os, const smanipiii& m) {
+    m.f(os, m.i1, m.i2, m.i3);  // call m’s function with m’s stored value i
+    return os;
+}
 
 }  // namespace manipulator
 
