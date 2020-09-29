df4277284589474d5f5001be6a09caa518b451b0
Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2020-09-29 18:04:13 +0800

    Relicense to MIT
    Closes #10

--- aesc/aesc.hpp
+++ aesc/aesc.hpp
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2019  Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
- *
- * Wrap some terminal control sequences with string streams
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope tha it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- */
-
 #ifndef AESC_AESC_HPP_
 #define AESC_AESC_HPP_
 
--- aesc/internal/manipulator.cpp
+++ aesc/internal/manipulator.cpp
@@ -1,21 +1,5 @@
 /*
- * Copyright (C) 2019  Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
- *
  * Manipulators of general output streams that take one or three arguments
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope tha it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  */
 
 #include "aesc/internal/manipulator.hpp"
